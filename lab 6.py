cc29934906191e92e442d7bb185821174a25b01b
Pereza22 <132220201+Pereza22@users.noreply.github.com>
2024-03-18 17:04:49 -0400

    Add files via upload

---
+++ lab 6.py
@@ -0,0 +1,39 @@
+def encode(password):
+    # This function encodes the password by shifting each digit up by 3 numbers.
+    return "".join(str((int(char) + 3) % 10) for char in password)
+
+
+def decode(encoded_password):
+    #partners function
+    return ""
+
+
+def main():
+    encoded_password = ""
+    original_password = ""
+    decoded_password = ""  # This will be used in option 2 to show the decoded password
+    while True:
+        print("Menu\n-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
+
+        if option == "1":
+            original_password = input("Please enter your password to encode: ")
+            encoded_password = encode(original_password)
+            print("Your password has been encoded and stored!")
+
+        elif option == "2":
+            if encoded_password:
+                decoded_password = decode(encoded_password)  # Your partner will fill in this function
+                print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
+            else:
+                print("No password has been encoded yet.")
+
+        elif option == "3":
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+main()
